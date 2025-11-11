88a4b2bd431314e5a7969d659bb7f6ed1b8618f8
Safeya-Yasien <ssafeya5@gmail.com>
2025-11-11 08:28:14 +0200

    build(deps): update server/index.ts, package-lock.json and package.json

--- server/index.ts
+++ server/index.ts
@@ -3,13 +3,16 @@ import express from "express";
 import customerRoutes from "./src/routes/customers.route";
 import cors from "cors";
 import connectDB from "./connectDB";
-// import serverless from "serverless-http";
 import path from "path";
 
 connectDB();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cutomer-management-react-node-prm5.vercel.app",
+];
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: allowedOrigins,
   optionsSuccessStatus: 200,
 };
 
@@ -20,16 +23,12 @@ const _dirname = path.resolve();
 
 app.use("/api/customers", customerRoutes);
 
-// const CLIENT_DIST_PATH = path.join(__dirname, "../client/dist");
 app.use(express.static(path.join(_dirname, "../client/dist")));
-// app.use(express.static(CLIENT_DIST_PATH));
 app.get("{*splat}", (_, res) => {
   res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
-  // res.sendFile(path.join(CLIENT_DIST_PATH, "index.html"));
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
-// export default serverless(app);
