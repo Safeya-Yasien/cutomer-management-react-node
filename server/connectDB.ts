d1cfcb373126027f77fc2b8090ba94d274361f2e
Safeya-Yasien <ssafeya5@gmail.com>
2025-11-11 09:24:41 +0200

    update connectDB.ts

--- server/connectDB.ts
+++ server/connectDB.ts
@@ -8,7 +8,6 @@ const connectDB = async (mongoURL: string) => {
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log("Error connecting to MongoDB:", err);
-    process.exit(1);
   }
 };
 
